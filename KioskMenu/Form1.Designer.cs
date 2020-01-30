7e11beb9f7a81042af7d6a50343b7ed6aeac611f
lh178-04 <lh178-04@sandhills.edu>
2020-01-30 12:23:38 -0500

    Initial Commit

--- KioskMenu/Form1.Designer.cs
+++ KioskMenu/Form1.Designer.cs
@@ -28,60 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.pictureEdit1 = new DevExpress.XtraEditors.PictureEdit();
-            this.pictureEdit2 = new DevExpress.XtraEditors.PictureEdit();
-            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.components = new System.ComponentModel.Container();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.mainPictureBox = new System.Windows.Forms.PictureBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnSlideShow = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit2.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
-            this.panelControl1.SuspendLayout();
+            this.cboSeconds = new System.Windows.Forms.ComboBox();
+            this.mainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureEdit1
+            // mainPanel
             // 
-            this.pictureEdit1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureEdit1.EditValue = ((object)(resources.GetObject("pictureEdit1.EditValue")));
-            this.pictureEdit1.Location = new System.Drawing.Point(2, 2);
-            this.pictureEdit1.Name = "pictureEdit1";
-            this.pictureEdit1.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
-            this.pictureEdit1.Size = new System.Drawing.Size(1260, 677);
-            this.pictureEdit1.TabIndex = 0;
+            this.mainPanel.Controls.Add(this.cboSeconds);
+            this.mainPanel.Controls.Add(this.label1);
+            this.mainPanel.Controls.Add(this.btnSlideShow);
+            this.mainPanel.Controls.Add(this.btnNext);
+            this.mainPanel.Controls.Add(this.btnPrevious);
+            this.mainPanel.Controls.Add(this.btnLoad);
+            this.mainPanel.Controls.Add(this.listBox1);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.mainPanel.Location = new System.Drawing.Point(799, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(465, 681);
+            this.mainPanel.TabIndex = 0;
             // 
-            // pictureEdit2
+            // mainPictureBox
             // 
-            this.pictureEdit2.EditValue = ((object)(resources.GetObject("pictureEdit2.EditValue")));
-            this.pictureEdit2.Location = new System.Drawing.Point(5, 2);
-            this.pictureEdit2.Name = "pictureEdit2";
-            this.pictureEdit2.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
-            this.pictureEdit2.Properties.Appearance.Options.UseBackColor = true;
-            this.pictureEdit2.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
-            this.pictureEdit2.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch;
-            this.pictureEdit2.Size = new System.Drawing.Size(346, 142);
-            this.pictureEdit2.TabIndex = 1;
+            this.mainPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.mainPictureBox.Name = "mainPictureBox";
+            this.mainPictureBox.Size = new System.Drawing.Size(799, 681);
+            this.mainPictureBox.TabIndex = 1;
+            this.mainPictureBox.TabStop = false;
             // 
-            // panelControl1
+            // timer1
             // 
-            this.panelControl1.Controls.Add(this.label1);
-            this.panelControl1.Controls.Add(this.pictureEdit2);
-            this.panelControl1.Controls.Add(this.pictureEdit1);
-            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelControl1.Location = new System.Drawing.Point(0, 0);
-            this.panelControl1.Name = "panelControl1";
-            this.panelControl1.Size = new System.Drawing.Size(1264, 681);
-            this.panelControl1.TabIndex = 2;
+            this.timer1.Interval = 3000;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(447, 251);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(37, 314);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(125, 37);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Load Image";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.BtnLoad_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Enabled = false;
+            this.btnPrevious.Location = new System.Drawing.Point(168, 314);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(64, 37);
+            this.btnPrevious.TabIndex = 2;
+            this.btnPrevious.Text = "<";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Enabled = false;
+            this.btnNext.Location = new System.Drawing.Point(238, 314);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(64, 37);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = ">";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // btnSlideShow
+            // 
+            this.btnSlideShow.Enabled = false;
+            this.btnSlideShow.Location = new System.Drawing.Point(37, 357);
+            this.btnSlideShow.Name = "btnSlideShow";
+            this.btnSlideShow.Size = new System.Drawing.Size(265, 37);
+            this.btnSlideShow.TabIndex = 4;
+            this.btnSlideShow.Text = "Slide Show";
+            this.btnSlideShow.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(97, 137);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 412);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(974, 86);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "SANDHILL\'S DOPE KIOSK MENU";
+            this.label1.Size = new System.Drawing.Size(132, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Interval in Seconds";
+            // 
+            // cboSeconds
+            // 
+            this.cboSeconds.FormattingEnabled = true;
+            this.cboSeconds.Location = new System.Drawing.Point(174, 409);
+            this.cboSeconds.Name = "cboSeconds";
+            this.cboSeconds.Size = new System.Drawing.Size(58, 21);
+            this.cboSeconds.TabIndex = 6;
             // 
             // Form1
             // 
@@ -89,26 +144,32 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.ClientSize = new System.Drawing.Size(1264, 681);
-            this.Controls.Add(this.panelControl1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Controls.Add(this.mainPictureBox);
+            this.Controls.Add(this.mainPanel);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit2.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
-            this.panelControl1.ResumeLayout(false);
-            this.panelControl1.PerformLayout();
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private DevExpress.XtraEditors.PictureEdit pictureEdit1;
-        private DevExpress.XtraEditors.PictureEdit pictureEdit2;
-        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.ComboBox cboSeconds;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSlideShow;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox mainPictureBox;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- KioskMenu/Form1.cs
+++ KioskMenu/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,8 @@ namespace KioskMenu
 {
     public partial class Form1 : Form
     {
+        string DirPath;
+        int ImgIndex;
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +22,66 @@ namespace KioskMenu
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            for (int i = 1; i < 10; i++)
+            {
+                cboSeconds.Items.Add(i);
+                cboSeconds.SelectedIndex = 0;
+            }
+        }
 
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = folderBrowserDialog1.ShowDialog();
+            DirPath = folderBrowserDialog1.SelectedPath;
+            string[] files = Directory.GetFiles(DirPath, "*.jpg");
+            foreach (string file in files)
+            {
+                int pos = file.LastIndexOf("||");
+                string FName = file.Substring(pos + 1);
+                listBox1.Items.Add(FName);
+            }
+            listBox1.SelectedIndex = ImgIndex = 0;
+            btnPrevious.Enabled = true;
+            btnNext.Enabled = btnSlideShow.Enabled = true;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            mainPictureBox.ImageLocation = listBox1.SelectedItem.ToString();
+            // pictureBox1.ImageLocation = Dirpath + "\\" + listBox1.SelectedItem;
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (ImgIndex > 0)
+            {
+                ImgIndex -= 1;
+                if (ImgIndex == 0)
+                {
+                    btnPrevious.Enabled = false;
+                }
+                if (ImgIndex < listBox1.Items.Count - 1)
+                    btnNext.Enabled = true;
+                listBox1.SelectedIndex = ImgIndex;
+            }
+        }
+
+        private void btnnext_Click(object sender, EventArgs e)
+        {
+            if (imgindex < listBox1.Items.Count - 1)
+            {
+                imgindex += 1;
+                if (imgindex == listBox1.Items.Count - 1)
+                    btnnext.Enabled = false;
+                if (imgindex > 0)
+                    btnprev.Enabled = true;
+                listBox1.SelectedIndex = imgindex;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            btnnext.PerformClick();
         }
     }
 }
